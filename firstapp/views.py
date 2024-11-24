a2590070a2c45a1534ecf68d40772fe2a7fb66f5
alibaghani <baghaniali2006@gmail.com>
2024-11-24 04:17:27 -0500

    feat: creat view for default subdomain www

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def default_subdomain_view(request):
+    return HttpResponse("no subdomains \n default host www")
